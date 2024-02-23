862042b8582980662314c57978de009e5be27e5d
serkanXman <msozkilic@gmail.com>
2024-02-23 17:52:00 +0100

    foreignCurrencySteps

--- src/test/java/Pages/dialogContent.java
+++ src/test/java/Pages/dialogContent.java
@@ -1,14 +1,20 @@
 package Pages;
 
 import Utilities.baseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class dialogContent extends parent{
 
 
     public dialogContent(){
+
         PageFactory.initElements(baseDriver.getDriver(),this);
     }
+
+    @FindBy(xpath = "//li[@id='pay_bills_tab']/a")
+    private WebElement paybills;
 
 
 
--- src/test/java/stepDefinitions/foreignCurrencySteps.java
+++ src/test/java/stepDefinitions/foreignCurrencySteps.java
@@ -1,4 +1,47 @@
 package stepDefinitions;
+import Pages.dialogContent;
+import io.cucumber.java.en.*;
 
 public class foreignCurrencySteps {
+
+    dialogContent dc=new dialogContent();
+
+    @When("Click login button")
+    public void click_login_button() {
+
+
+    }
+    @Then("Click to Pay Bills button")
+    public void click_to_pay_bills_button() {
+
+    }
+    @Then("Click to Purchase Foreign Currency button")
+    public void click_to_purchase_foreign_currency_button() {
+
+    }
+    @Then("select currency as {string}")
+    public void select_currency_as(String string) {
+
+    }
+    @Then("Enter to Amount as {string}")
+    public void enter_to_amount_as(String string) {
+
+    }
+    @Then("Click to Dollar checkbox")
+    public void click_to_dollar_checkbox() {
+
+    }
+    @Then("Click to calculate")
+    public void click_to_calculate() {
+
+    }
+    @Then("Click to purchase")
+    public void click_to_purchase() {
+
+    }
+    @Then("successfully message should be displayed")
+    public void successfully_message_should_be_displayed() {
+
+    }
+
 }
